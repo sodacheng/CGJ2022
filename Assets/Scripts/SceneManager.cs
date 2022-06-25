20fd26aa88171ad9ce49870cb759fc7b05574293
橙飒 <634377685@qq.com>
2022-06-25 14:41:27 +0800

    Update SceneManager.cs

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -38,7 +38,7 @@ public class SceneManager : MonoBehaviour
     {
         score++;
         
-        // TODO 更新UI分数显示
+        //更新UI分数显示
         UIManager.Instance.GetPanel<ScenePanel>().UpdateScore(score); // 失败时更新生命同理
         
         
